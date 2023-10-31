5a5218a2c5c6532df4592d873a37b44a3a1c95ee
genie-ru <genie.zgmf987@gmail.com>
2023-10-31 18:30:57 +0900

    test

--- chap_2/scripts/hello_ex.js
+++ chap_2/scripts/hello_ex.js
@@ -1,1 +1,13 @@
 console.log("こんにちは");
+
+let member = '佐藤理央,女25,rio@example.com';
+let gender = member.split(',')[1];
+// if (gender === '女') { ... }
+
+let price = 100;
+console.log(price + 1.1);
+
+// 配列
+let data = ['JavaScript', 'Python', 'PHP', 'Ruby', 'Perl'];
+
+console.log(data[0]);
