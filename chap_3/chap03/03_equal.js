1a3e406e8a580cd01787e42f74981bfd1d690dde
genie-ru <genie.zgmf987@gmail.com>
2023-11-09 07:42:55 +0900

    test

---
+++ chap_3/chap03/03_equal.js
@@ -0,0 +1,11 @@
+let data1 = [0, 1, 2];
+let data2 = data1;
+data1 = [10, 20, 30];
+console.log(data2);
+// Output: [0, 1, 2]
+
+const TAX = 1.1;
+
+const data = [1, 2, 3];
+data = [4, 5, 6];
+data[1] = 10;
