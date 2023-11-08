8ffe5239f63ac4ebcb424f2724a4b660ab84efb9
genie-ru <genie.zgmf987@gmail.com>
2023-11-09 07:31:34 +0900

    test

---
+++ chap_3/chap03/01_plus.js
@@ -0,0 +1,13 @@
+console.log('Java' + 'Script');
+console.log('10' + '1');
+console.log(5 + '6');
+// let today = new Data();
+
+// console.log(1234 + today);
+console.log(1 + '2' + 3);
+console.log(1 + 3 + '2');
+
+let x = 3.4;
+let y = 4;
+
+console.log(Math.round(x+y) / 100);
